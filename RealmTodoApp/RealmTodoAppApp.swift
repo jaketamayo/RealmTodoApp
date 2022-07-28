80a5a3bd57c683a64198ebd71d3b75987bd43146
jaketamayo <hellojtmatthew@icloud.com>
2022-07-28 15:47:40 -0500

    Initial Commit

---
+++ RealmTodoApp/RealmTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmTodoAppApp.swift
+//  RealmTodoApp
+//
+//  Created by Jacob Tamayo on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct RealmTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
